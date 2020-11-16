a60254dbee05b4e040d87eb74753cf57c2c0cbdb
Alessandro Clerici <alessandro.clericilorenzini@studenti.unimi.it>
2020-11-16 18:39:23 +0100

    new command

--- setPath.bash
+++ setPath.bash
@@ -6,3 +6,6 @@ fi
 
 # source this file to update your PATH bash environment variable
 export PATH=$PATH:~/uni/nc_prog2/repo/.bin
+
+# add a stupid alias too
+alias testall='find -name "Test*.java" -or -iname "soluzione.java" -execdir verifica \;'
