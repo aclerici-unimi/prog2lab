ee3b679bed23ff4790966d4ac66f6f41edd4ab29
Alessandro Clerici <alessandro.clericilorenzini@studenti.unimi.it>
2020-11-24 00:44:40 +0100

    added t10/e04

---
+++ t10/04-intqueue_unbounded_elements/Test.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		IntQueue q = new IntQueue();
+		if (sc.hasNextInt())
+			q.enqueue(sc.nextInt());
+		for (int i = 1; sc.hasNextInt(); i = ++i % 3)
+			if (i % 3 == 0)
+				q.dequeue();
+			else
+				q.enqueue(sc.nextInt());
+		for (Iterator<Integer> it = q.elements(); it.hasNext();)
+			System.out.println(it.next());
+		sc.close();
+	}
+}
